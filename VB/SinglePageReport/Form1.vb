75303e310d098e8f41307fbe6ba244b20aad087c
DevExpressExampleBot <fakeEmail>
2022-11-24 01:03:39 +0400

    Source auto update [skip ci]

--- VB/SinglePageReport/Form1.vb
+++ VB/SinglePageReport/Form1.vb
@@ -1,37 +1,31 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraReports.UI
 
 Namespace SinglePageReport
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			Dim list As New List(Of Data)()
-			Dim random As New Random()
+    Public Partial Class Form1
+        Inherits Form
 
-			For i As Integer = 0 To 499
-				Dim data As New Data() With {.Name = String.Format("Name{0}", i), .Group = String.Format("Group{0}", random.Next(10)), .Description = String.Format("Item Description {0}", i), .Value = random.Next(1000)}
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-				list.Add(data)
-			Next i
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim list As List(Of Data) = New List(Of Data)()
+            Dim random As Random = New Random()
+            For i As Integer = 0 To 500 - 1
+                Dim data As Data = New Data() With {.Name = String.Format("Name{0}", i), .Group = String.Format("Group{0}", random.Next(10)), .Description = String.Format("Item Description {0}", i), .Value = random.Next(1000)}
+                list.Add(data)
+            Next
 
-			Dim report As New SampleReport() With {.DataSource = list}
-
-			SinglePageHelper.GenerateSinglePageReport(report)
-
-			Dim printTool As New ReportPrintTool(report)
-			printTool.ShowPreviewDialog()
-		End Sub
-	End Class
+            Dim report As SampleReport = New SampleReport() With {.DataSource = list}
+            GenerateSinglePageReport(report)
+            Dim printTool As ReportPrintTool = New ReportPrintTool(report)
+            printTool.ShowPreviewDialog()
+        End Sub
+    End Class
 End Namespace
